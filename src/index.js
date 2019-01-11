131081536f42a6fe67b55f895d0ca3495fea4148
John <john.19col@gmail.com>
2019-01-10 23:11:14 -0500

    Render App component provided with the redux store

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+import reducers from './state/reducers';
+import middlewares from './state/middlewares';
+
+import './index.css';
+
+const store = createStore(reducers, middlewares);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
