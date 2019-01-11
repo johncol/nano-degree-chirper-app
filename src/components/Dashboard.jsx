ec4ff9ccdf49035c506a1bb7c9db27ce9985c93e
John <john.19col@gmail.com>
2019-01-11 11:54:12 -0500

    Update Dashboard to display tweets using Tweet component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import TweetsSelector from '../state/selectors/tweets';
+import Tweet from './Tweet';
 
 class Dashboard extends Component {
   render() {
     const { tweetIds } = this.props;
     return (
       <section>
-        <h1>Your timeline</h1>
-        {tweetIds.map(tweetId => (
-          <div key={tweetId}>Tweet Id: {tweetId}</div>
-        ))}
+        <h3 className="center">Your timeline</h3>
+        <ul className="dashboard-list">
+          {tweetIds.map(tweetId => (
+            <li key={tweetId}>
+              <Tweet tweetId={tweetId} />
+            </li>
+          ))}
+        </ul>
       </section>
     );
   }
