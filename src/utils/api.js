673ba2fc47f3014777b0e392d54130d4ae5f50d0
John <john.19col@gmail.com>
2019-01-10 23:06:23 -0500

    Add mock API

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import { _getUsers, _getTweets, _saveLikeToggle, _saveTweet } from './_DATA.js';
+
+export function getInitialData() {
+  return Promise.all([_getUsers(), _getTweets()]).then(([users, tweets]) => ({
+    users,
+    tweets,
+    authedUser: 'sarah_edo'
+  }));
+}
+
+export function saveLikeToggle(info) {
+  return _saveLikeToggle(info);
+}
+
+export function saveTweet(info) {
+  return _saveTweet(info);
+}
