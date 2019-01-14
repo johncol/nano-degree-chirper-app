9a5078fa92b349287a445fbaaf3feb74abee0ee6
John <john.19col@gmail.com>
2019-01-14 15:37:28 -0500

    Add navigation to the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoadingBar from 'react-redux-loading';
-import { Switch, Route } from 'react-router-dom';
 
 import { SharedActionCreator } from '../state/actions/shared';
 import SharedSelector from '../state/selectors/shared';
+
 import Dashboard from './Dashboard';
 import NewTweet from './NewTweet';
 import TweetPage from './TweetPage';
 import NotFound from './NotFound';
+import Navigation from './Navigation';
 
 class App extends Component {
   componentDidMount() {
@@ -18,18 +20,23 @@ class App extends Component {
   render() {
     const { loading } = this.props;
     return (
-      <div>
-        <LoadingBar />
-        {!loading && (
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/tweets/new" component={NewTweet} />
-            <Route path="/tweets/:id" component={TweetPage} />
-            <Route component={NotFound} />
-          </Switch>
-        )}
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Navigation />
+            {!loading && (
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/tweet/new" component={NewTweet} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route component={NotFound} />
+              </Switch>
+            )}
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 import reducers from './state/reducers';
@@ -13,10 +12,8 @@ import './index.css';
 const store = createStore(reducers, middlewares);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
