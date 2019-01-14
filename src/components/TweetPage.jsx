63fee103188bf72d70ee3d9754039e607339b7c8
John <john.19col@gmail.com>
2019-01-14 15:40:07 -0500

    Improve TweetPage component readability by creating TweetReplies

--- src/components/TweetPage.jsx
+++ src/components/TweetPage.jsx
@@ -5,23 +5,34 @@ import Tweet from './Tweet';
 import NewTweet from './NewTweet';
 import TweetsSelector from '../state/selectors/tweets';
 
+const TweetReplies = ({ replyIds }) => {
+  if (replyIds.length === 0) {
+    return null;
+  }
+
+  return (
+    <React.Fragment>
+      <h3 className="center">Replies</h3>
+      <ul>
+        {replyIds.map(replyId => (
+          <li key={replyId}>
+            <Tweet id={replyId} />
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
 class TweetPage extends Component {
   render() {
     const { id, replyIds } = this.props;
     return (
-      <div>
+      <React.Fragment>
         <Tweet id={id} />
         <NewTweet replyingTo={id} />
-
-        {replyIds.length > 0 && (
-          <div>
-            <h3 className="center">Replies</h3>
-            {replyIds.map(replyId => (
-              <Tweet key={replyId} id={replyId} />
-            ))}
-          </div>
-        )}
-      </div>
+        <TweetReplies replyIds={replyIds} />
+      </React.Fragment>
     );
   }
 }
